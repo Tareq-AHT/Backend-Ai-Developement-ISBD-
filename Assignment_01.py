7b4c02ca6e364d2a17bdfdca4cab5ee588dc7bb4
Tareq Al Hossain Tomal <67488643+Tareq-AHT@users.noreply.github.com>
2025-10-12 16:16:12 +0600

    Add files via upload

---
+++ Assignment_01.py
@@ -0,0 +1,9 @@
+# Assignment 01
+# input a number from user
+# convert the number from number to string
+# print the converted number and it's datatype
+
+
+num = int(input("Please enter your desire number: "))
+con_num = str(num)
+print("Converter number to string:", con_num, type(con_num))
